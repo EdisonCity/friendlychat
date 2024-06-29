5d4e36b187b8220eb12e3e1270295eb4f6dea170
EdisonCity <yshdtkeshi@gmail.com>
2024-06-29 23:54:39 +0900

    Install AngularFire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -19,6 +19,6 @@ export const appConfig: ApplicationConfig = {
       provideStorage(() => getStorage()),
       provideMessaging(() => getMessaging())
     ),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendchat-9df35","appId":"1:1086407042999:web:15d4c64d63b20faa852a32","storageBucket":"friendchat-9df35.appspot.com","locationId":"asia-northeast1","apiKey":"AIzaSyCpE1U897jFMOaVx0c5HHKiGOVpst9Z1qU","authDomain":"friendchat-9df35.firebaseapp.com","messagingSenderId":"1086407042999"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
 };
